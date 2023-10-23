52c410d67090f295d0e46c2743a3dba41f03e1e2
Kim yeonsu <yeonsu0329@kyonggi.ac.kr>
2023-10-23 17:36:09 +0900

    faceCheck server 생성

---
+++ faceCheck-server/src/main/java/com/dcs/faceCheckserver/FaceCheckServerApplication.java
@@ -0,0 +1,13 @@
+package com.dcs.faceCheckserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FaceCheckServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FaceCheckServerApplication.class, args);
+	}
+
+}
