5ea5205730fe9b7ce4187bdc15d29c4d0772cb47
Kim yeonsu <yeonsu0329@kyonggi.ac.kr>
2024-03-03 18:55:03 +0900

    feat: 이미지 출력 - byte[] 반환

--- faceCheck-server/src/main/java/com/dcs/faceCheckserver/image/ImageService.java
+++ faceCheck-server/src/main/java/com/dcs/faceCheckserver/image/ImageService.java
@@ -1,16 +1,25 @@
 package com.dcs.faceCheckserver.image;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Base64;
 import java.util.List;
 import java.util.UUID;
 
 @Service
 public class ImageService {
     private final ImageRepository imageRepository;
+
+    @Value("${image.upload.directory}")
+    private String uploadDirectory;
 
     public ImageService(ImageRepository imageRepository) {
         this.imageRepository = imageRepository;
@@ -35,7 +44,7 @@ public class ImageService {
 
             // 파일을 불러올 때 사용할 파일 경로
             // 경로 설정해야 함
-            String savedPath = "/Users/yeonsu/IdeaProjects/faceCheck/img" + savedName;
+            String savedPath = uploadDirectory + savedName;
 
             // 파일 엔티티 생성
             Image image = Image.builder()
@@ -54,4 +63,49 @@ public class ImageService {
         }
         return imageIdList;
     }
+
+    public ResponseEntity<byte[]> getImage(Long imageId) {
+        try {
+            byte[] imageData = getImageById(imageId);
+            String encodedImageData = Base64.getEncoder().encodeToString(imageData);
+            System.out.println(encodedImageData);
+            return ResponseEntity.ok().body(imageData);
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(null);
+        }
+    }
+
+    private byte[] getImageById(Long imageId) throws Exception {
+        // 이미지 ID로 데이터베이스에서 이미지 데이터를 가져오는 로직
+        Image image = imageRepository.findByImageId(imageId);
+        // 이미지 데이터를 바이트 배열로 변환하여 반환
+        return getImageDataAsByteArray(image);
+    }
+
+    private byte[] getImageDataAsByteArray(Image image) {
+        try {
+            // 이미지 파일을 읽어들이는 FileInputStream 생성
+            FileInputStream fileInputStream = new FileInputStream(image.getSavedPath());
+
+            // 이미지 파일의 크기를 구함
+            long fileSize = image.getSavedPath().length();
+
+            // 이미지 파일을 저장할 바이트 배열 생성
+            byte[] imageData = new byte[(int) fileSize];
+
+            // 이미지 파일을 바이트 배열에 읽어들임
+            int read = fileInputStream.read(imageData);
+            System.out.println(read);
+
+            // FileInputStream 닫기
+            fileInputStream.close();
+
+            return imageData;
+        } catch (IOException e) {
+            // 파일 읽기 실패 시 예외 처리
+            e.printStackTrace();
+            return null;
+        }
+    }
+
 }
