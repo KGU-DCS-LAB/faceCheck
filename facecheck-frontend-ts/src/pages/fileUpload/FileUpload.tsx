eee545a269ff7d54f1ed313f5850ea36e250018b
seeun <ssky601@gmail.com>
2024-04-07 19:52:11 +0900

    feat: 파일 업로드

--- facecheck-frontend-ts/src/pages/fileUpload/FileUpload.tsx
+++ facecheck-frontend-ts/src/pages/fileUpload/FileUpload.tsx
@@ -2,15 +2,49 @@ import React from "react";
 import Dropzone, {useDropzone} from "react-dropzone";
 import CloudUploadIcon from '@material-ui/icons/CloudUpload'
 import {Box, Typography} from "@mui/material";
+import Axios, {AxiosResponse} from "axios";
+import Cookies from "js-cookie";
+import Swal from "sweetalert2";
 
 const FileUpload:React.FC = () => {
 
-    const  { getRootProps , getInputProps }  =  useDropzone ( )
+    const dropHandler = (files: File[]) => {
+        let formData = new FormData();
+
+        files.forEach(file => {
+            formData.append("files", file);
+        });
+        console.log(formData.get("files"))
+
+        Axios.post("/image/upload", formData, {
+            headers: {
+                "Authorization": `Bearer ${Cookies.get("accessToken")}`,
+                "content-type": "multipart/form-data",
+            },
+        }).then((response: AxiosResponse<number[]>) => {
+            if(response.status === 200) {
+                console.log(response.data);
+                Swal.fire({
+                    icon: "success",
+                    title: "이미지 업로드 성공",
+                    text: "성공적으로 업로드 되었습니다.",
+                });
+            }
+        }).catch((error: any) => {
+            if(error.response.status === 400) {
+                Swal.fire({
+                    icon: "error",
+                    title: "이미지 업로드 실패",
+                    text: error.response.data,
+                });
+            }
+        })
+    }
 
     return  (
         <Dropzone
-            // onDrop
-            multiple = {false}  //false는 파일 하나, true는 파일 여러개
+            onDrop={dropHandler}
+            multiple = {true}  //false는 파일 하나, true는 파일 여러개
             maxSize = {1000000000}
         >
             {({ getRootProps, getInputProps }) => (
