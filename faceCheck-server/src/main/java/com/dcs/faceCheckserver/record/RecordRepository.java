adf28e1d8decad6f66afe696115c2f721ccf4bc5
Kim yeonsu <yeonsu0329@kyonggi.ac.kr>
2024-07-11 01:26:16 +0900

    feat: 직원 출입 기록 조회

--- faceCheck-server/src/main/java/com/dcs/faceCheckserver/record/RecordRepository.java
+++ faceCheck-server/src/main/java/com/dcs/faceCheckserver/record/RecordRepository.java
@@ -3,5 +3,8 @@ package com.dcs.faceCheckserver.record;
 import com.dcs.faceCheckserver.record.data.Record;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface RecordRepository extends JpaRepository<Record, Long> {
+    List<Record> findByEmployeeIsNotNull();
 }
